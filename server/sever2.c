5d78903b4a726e6d8a40eb56c9d2d7fb12017752
Pablo Flores <41632182+mutyart@users.noreply.github.com>
2021-04-26 11:05:23 -0300

    primer commit

---
+++ server/sever2.c
@@ -0,0 +1,24 @@
+/*
+	Initialise Winsock
+*/
+
+#include<stdio.h>
+#include<winsock2.h>
+
+#pragma comment(lib,"ws2_32.lib") //Winsock Library
+
+int main(int argc , char *argv[])
+{
+	WSADATA wsa;
+
+	printf("\nInitialising Winsock...");
+	if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+	{
+		printf("Failed. Error Code : %d",WSAGetLastError());
+		return 1;
+	}
+
+	printf("Initialised.");
+
+	return 0;
+}
